05d8e91e9340247ed37ce49de34055470a5ceaf2
Param Arora <SP@Params-MacBook-Air.local>
2023-07-29 17:30:00 +0530

    first commit

---
+++ src/components/RollDice.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+const RollDice = ({roleDice,currentDice}) => {
+
+
+  return (
+    <DiceContainer>
+    <div className="dice"
+    onClick={roleDice}>
+        <img src={`/images/dice_${currentDice}.png`} alt="dice_1"></img>
+    </div>
+    <p>Click on Dice to Roll</p>
+    </DiceContainer> 
+  )
+}
+
+export default RollDice 
+
+const DiceContainer = styled.div`
+    margin-top:48px;
+    display: flex;
+    flex-direction:column;
+    align-items:center; 
+
+    .dice{
+        cursor:pointer;
+    }
+
+    p{
+        font-size:24px;
+    }
+`;
+
